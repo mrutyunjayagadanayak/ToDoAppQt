116d39579246f3110557669cc1a1762d65d845f0
Mrutyunjaya Gadanayak <mrutyunjayagadanayak@outlook.com>
2023-09-10 15:27:06 +0530

    changed access modifier for config path

--- fileops.h
+++ fileops.h
@@ -8,6 +8,7 @@ class FileOps
 {
 private:
     void initiate_dir();
+    const static QString main_config_path;
 public:
     bool read_file(Ui::MainWindow *ui, QString path=main_config_path);
     bool write_file(Ui::MainWindow *ui,QString path=main_config_path);
@@ -16,7 +17,7 @@ public:
     bool delete_file(QString path);
     FileOps();
     const static QString dir_path;
-    const static QString main_config_path;
+
 };
 
 #endif // FILEOPS_H
