15ce69ef9538433f6758a70ab6f4e41b608cfbf0
bapun <mrutyunjayagadanayak@yandex.com>
2023-09-10 15:28:27 +0530

    New UI window (#2)
    * Added a textedit for todo items
    
    * changed access modifier for config path
    
    ---------
    
    Co-authored-by: Mrutyunjaya Gadanayak <mrutyunjayagadanayak@outlook.com>

--- fileops.h
+++ fileops.h
@@ -7,13 +7,17 @@
 class FileOps
 {
 private:
-    const static QString dir_path;
-    const static QString main_config_path;
     void initiate_dir();
+    const static QString main_config_path;
 public:
     bool read_file(Ui::MainWindow *ui, QString path=main_config_path);
     bool write_file(Ui::MainWindow *ui,QString path=main_config_path);
+    bool write_todo(Ui::MainWindow *ui,QString path=main_config_path);
+    bool read_todo(Ui::MainWindow *ui,QString path=main_config_path,QString item_name="");
+    bool delete_file(QString path);
     FileOps();
+    const static QString dir_path;
+
 };
 
 #endif // FILEOPS_H
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -29,6 +29,7 @@ void MainWindow::on_button_add_clicked()
                                                     ui->listWidget);
         ui->listWidget->addItem(item);
         item->setFlags(item->flags() | Qt::ItemIsEditable);
+        //item->setCheckState(Qt::Unchecked);
     }
     ui->txt_task->clear();
     ui->txt_task->setFocus();
@@ -38,11 +39,33 @@ void MainWindow::on_button_add_clicked()
 void MainWindow::on_button_remove_clicked()
 {
     QListWidgetItem* item = ui->listWidget->takeItem(ui->listWidget->currentRow());
+    FileOps delete_file;
+    delete_file.delete_file(FileOps::dir_path + "/" +item->text());
+    ui->textEdit->clear();
     delete item;
 }
 
 
 void MainWindow::on_button_remove_all_clicked()
 {
+    FileOps delete_file;
+    for(size_t i{0};i < ui->listWidget->count();i++)
+    {
+        delete_file.delete_file(FileOps::dir_path + "/" + ui->listWidget->item(i)->text());
+    }
     ui->listWidget->clear();
+    ui->textEdit->clear();
 }
+
+void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item)
+{
+    FileOps change_todo_item;
+    change_todo_item.read_todo(ui,FileOps::dir_path + "/" +item->text(),item->text());
+}
+
+void MainWindow::on_save_todo_clicked()
+{
+    FileOps todo_file;
+    todo_file.write_todo(ui,FileOps::dir_path + "/" + ui->listWidget->currentItem()->text());
+}
+
